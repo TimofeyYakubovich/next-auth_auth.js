f0e9e0340df8910acd73b3822f9da754837fc0ab
Timofey <yakubovich96@mail.ru>
2024-07-25 22:35:24 +0300

    fix useSearchParams

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,12 +1,15 @@
 import { GoogleButton } from "@/components/GoogleButton";
 import { SignInForm } from "@/components/SignInForm";
+import { Suspense } from "react";
 
 // кастомная страница входа
 export default async function Signin() {
     return (
         <div className="stack">
           <h1>SignIn</h1>
-          <GoogleButton />
+          <Suspense fallback={<div>Loading...</div>}>
+            <GoogleButton />
+          </Suspense>
           <div>or</div>
           <SignInForm />
         </div>
